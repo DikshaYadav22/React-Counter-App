6fd7751da5efe0c2885acfd143acc396a45a195b
DikshaYadav22 <diksha.yadav022@gmail.com>
2022-01-18 17:52:32 +0530

    modified style css

--- src/component/Tempapp.jsx
+++ src/component/Tempapp.jsx
@@ -5,38 +5,45 @@ import './css/style.css';
 const Tempapp = () => {
 
     const[city, setCity] = useState(null);
-    const[search, setSearch] = useState("Dehradun");
+    const[search, setSearch] = useState("");
 
     const searchData = (event) => {
         setSearch(event.target.value);
 
     }
-
+       
     useEffect(() => {
+        console.log("useEffect called");
         const fetchApi = async() => {
-            const response = 
-        await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a7bd79a9a457cc35acf165b0050d5cf1`);
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a7bd79a9a457cc35acf165b0050d5cf1`);
             if(response.data){
                 console.log(response.data);
                 setCity(response.data.main);
+            }else{
+                console.log(response.message);
             }
         }
-
-        fetchApi();
+       fetchApi();
     }, [search])
    
 
     return(
         
-        <>
+        <>   
+            <marquee behavior="scroll"  scrollamount="3">
+                <h2 className='text-center p-4'>React Live Weather Search</h2>
+            </marquee>
             <div className='box'>
+             
                 <div className="inputData">
                     <input type="search" 
                             className='inputField'
                             onChange = {searchData}
                     />
                 </div>
-                { !city ? (<p> Data not found </p> )
+                {!city ? (
+                    <p> City not found </p> 
+                )
                     :
                 (
                     <div>
@@ -47,11 +54,11 @@ const Tempapp = () => {
                     <h1 className = "temp">
                        {city.temp}^Cel
                     </h1>
-                    <h3>Min : {city.temp_min}^Cel | Max : {city.temp_min}^Cel</h3>
+                    <h3 className='tempmin_max'>Min : {city.temp_min}^Cel | Max : {city.temp_min}^Cel</h3>
                 </div>
-                <div className="wave -one"></div>
-                <div className="wave -two"></div>
-                <div className="wave -three"></div>
+                <div className="wave-one"></div>
+                <div className="wave-two"></div>
+                <div className="wave-three"></div>
                 </div>
                 )
 }
